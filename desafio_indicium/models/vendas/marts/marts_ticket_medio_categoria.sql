87a182e98789aa46f9420cb1477e09ea3e7ed63f
Juan Mangueira <juanmangueiraa@gmail.com>
2023-06-08 15:24:29 -0300

    Update ticket medio

--- desafio_indicium/models/vendas/marts/marts_ticket_medio_categoria.sql
+++ desafio_indicium/models/vendas/marts/marts_ticket_medio_categoria.sql
@@ -1,9 +1,10 @@
 SELECT
-    ca.categoria_id,
-    ca.categoria_nome,
-    (fc.valor_total_categoria/fc.quantidade_total_categoria) AS ticket_medio
-FROM {{ref('stg_categorias')}} AS ca
-INNER JOIN
-{{ref('int_vendas_categoria')}} AS fc
-ON
-ca.categoria_id=fc.categoria_id
+    categoria_id,
+    categoria_nome,
+    (SUM(valor_total_categoria)/SUM(quantidade_total_categoria)) AS ticket_medio
+FROM {{ref('int_vendas_categoria_detalhado')}}
+GROUP BY
+    categoria_id,
+    categoria_nome
+ORDER BY
+    ticket_medio DESC
